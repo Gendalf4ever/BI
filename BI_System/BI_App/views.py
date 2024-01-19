bba1b2c801c743e2ac8387fc5287fdec7d767241
Nikita Yurishev <chelovek438@gmail.com>
2024-01-19 17:13:15 +0300

    login added

--- BI_System/BI_App/views.py
+++ BI_System/BI_App/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse 
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 def home(request): 
     return render(request, 'BI_App/home.html')
 
 
 def login(request):
-    return render(request, 'BI_App/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, "login success")
+            return redirect('BI_App/home.html')
+        else:
+            messages.warning(request, "error, please try again")
+            return redirect('BI_App/login.html')
+
+        # else:
+            # return render(request, 'BI_App/login.html')
+         
+        
+        
